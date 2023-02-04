d5fae4c94e1bf9ee620160920faacf7b6491a106
João Vitor <devjohncb@gmail.com>
2023-02-04 16:56:35 -0300

    added axios and cep-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import { FiSearch } from "react-icons/fi";
 
+import api from "./services/api";
+
 function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  // const handleInputChange = (event) => {
+  //   setInput(event.target.value);
+  // } => sem utilizar desestruturação
+
+  // => utilizando desestruturação
+  const handleInputChange = ({ target }) => {
+    setInput(target.value);
+  };
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Favor, preencher o campo com algum CEP");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Opsss!! Erro ao buscar o cep...");
+      setInput("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite um cep..." />
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Digite um cep..."
+          value={input} // o value sempre será o estado
+          onChange={handleInputChange}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
 
-      <main className="mainContainer">
-        <h2>CEP: 24456-435</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className="mainContainer">
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua Justo Brito Snaches</span>
-        <span>Complemento: Algum Complemento</span>
-        <span>Trindade</span>
-        <span>São Gonçalo - RJ</span>
-      </main>
+          <span>{cep.logradouro}</span>
+          {cep.complemento === "" || (<span>Complemento: {cep.complemento}</span>)}
+          <span>Bairro: {cep.bairro}</span>
+          <span>Localidade: {cep.localidade} - {cep.uf}</span>
+
+        </main>
+      )}
     </div>
   );
 }
